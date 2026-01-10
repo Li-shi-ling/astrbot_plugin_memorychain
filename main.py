e5ecfcd20ba3b888fa8ba560304036257a18e81b
Lishiling <m18384519631@163.com>
2026-01-10 13:58:23 +0800

    init:初始化代码

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+# LLM 对话类
+from astrbot.api.event import filter, AstrMessageEvent
+from astrbot.core.conversation_mgr import Conversation
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.api import AstrBotConfig, logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+@register("memorychain", "Lishining", "记忆链", "1.0.0")
+class memorychain(Star):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.Config = config
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+    @filter.command_group("memorychain")
+    def memorychain(self):
+        pass
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+    @memorychain.command("cs")
+    async def cs(self, event: AstrMessageEvent):
+        data = self.context.kb_manager.list_kbs()
+        yield event.plain_result(f"data:{data}")
+        logger.info(f"data:{data}")
